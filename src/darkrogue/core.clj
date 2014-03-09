cc61166f3b70c133fb531ccb1e4f2bd65082ac25
Michael Heasell <michael.heasell@gmail.com>
2014-03-09 21:01:23 +0100

    Add simple initial screen

--- src/darkrogue/core.clj
+++ src/darkrogue/core.clj
@@ -1,1 +1,20 @@
 (ns darkrogue.core)
+
+(require '[lanterna.screen :as s])
+
+(defn main [screen-type]
+  (let [screen (s/get-screen screen-type)]
+    (s/in-screen screen
+                 (s/put-string screen 0 0 "DarkRogue" {:styles #{:bold}})
+                 (s/put-string screen 0 1 "A RogueLike game by Michael Heasell")
+                 (s/put-string screen 0 2 "Press any key to exit...")
+                 (s/redraw screen)
+                 (s/get-key-blocking screen))))
+
+(defn -main [& args]
+  (let [args (set args)
+        screen-type (cond
+                      (args ":swing") :swing
+                      (args ":text") :text
+                      :else :auto)]
+    (main screen-type)))
