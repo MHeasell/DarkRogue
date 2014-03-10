cb6abf7046cddc05204c8b546d02410e4e71115b
Michael Heasell <michael.heasell@gmail.com>
2014-03-10 21:18:09 +0100

    Add a bunch of graph and grid functions

--- src/darkrogue/core.clj
+++ src/darkrogue/core.clj
@@ -2,6 +2,53 @@
 
 (require '[lanterna.screen :as s])
 
+
+; world  data
+
+(defrecord Coordinate [x y])
+
+(defrecord Grid [width height elements defaultval])
+
+(defn make-grid [width height value]
+  "Creates a grid of the given width and height, filled with the given value."
+  (Grid. width height {} value))
+
+(defn get-cell
+  ([grid coord]
+    (let [val (get (:elements grid) coord)]
+      (if val
+        val
+        (:defaultval grid))))
+  ([grid x y]
+    (get-cell grid (Coordinate. x y))))
+
+
+; graph routines
+
+(defn graph [& items]
+  (make-graph items))
+
+(defn make-graph [coll]
+  (reduce add-node empty-graph coll))
+
+(def empty-graph {})
+
+(defn add-directed-edge [graph n1 n2]
+  (let [n1-neighbours (n1 graph)]
+    (assoc graph n1 (conj n1-neighbours n2))))
+
+(defn add-edge [graph n1 n2]
+  (-> graph
+    (add-directed-edge n1 n2)
+    (add-directed-edge n2 n1)))
+
+(defn add-node [graph val]
+  (assoc graph val #{}))
+
+(defn connected? [graph n1 n2]
+  (contains? (n1 graph) n2))
+
+
 (defn main [screen-type]
   (let [screen (s/get-screen screen-type)]
     (s/in-screen screen
--- test/darkrogue/core_test.clj
+++ test/darkrogue/core_test.clj
@@ -2,6 +2,24 @@
   (:require [clojure.test :refer :all]
             [darkrogue.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-make-graph
+  (testing "check that making a graph works"
+           (is (= {:a #{} :b #{}}
+                  (graph :a :b)))))
+(deftest test-make-graph
+  (testing "checks that making a graph works"
+           (is (= {:a #{} :b #{} :c #{}}
+                  (make-graph '(:a :b :c))))))
+
+(deftest test-connected
+  (testing "checks that connected predicate works"
+           (is (false?
+                 (connected? (graph :a :b) :a :b)))
+           (is (true?
+                 (connected? (add-edge (graph :a :b) :a :b) :a :b)))))
+
+(deftest test-grid
+  (testing "checks that grid get/set works"
+           (is (= 0 (get-cell (make-grid 2 2 0) 0 0)))))
+
+(run-tests)
