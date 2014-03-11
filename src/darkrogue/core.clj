a582c660edcc54541853fd37d8f7d2dddb080f81
Michael Heasell <michael.heasell@gmail.com>
2014-03-11 20:10:09 +0100

    Add function to generate the world graph

--- src/darkrogue/core.clj
+++ src/darkrogue/core.clj
@@ -131,6 +131,27 @@
           candidate))
       graph)))
 
+(defn connect-initial-chain [graph]
+  (connect-nodes-from graph (rand-nth (nodes graph))))
+
+(defn connect-remaining-nodes [graph]
+  (let [unconnected (remove (partial connected-to-any? graph) (nodes graph))]
+    (if (seq unconnected)
+      (let [node-set (set (nodes graph))
+            candidate (rand-nth unconnected)
+            bros (filter #(contains? node-set %) (neighbours candidate))
+            connected-bros (filter #(connected-to-any? graph %) bros)]
+        (if (seq connected-bros)
+          (connect-remaining-nodes (add-edge graph candidate (rand-nth connected-bros)))
+          (connect-remaining-nodes graph))
+        )
+      graph)))
+
+(defn generate-world-graph
+  (-> (make-graph (coords-in-rect 0 0 3 3))
+    (connect-initial-chain)
+    (connect-remaining-nodes)
+    (add-random-connections (rand-int 3))))
 
 ; main screen displaying stuff
 
