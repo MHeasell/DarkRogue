0de9f826b512e00c47d67dba4c375c16c5030db1
Michael Heasell <michael.heasell@gmail.com>
2014-03-12 20:28:21 +0100

    Make project runnable by "lein run"

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clojure-lanterna "0.9.4"]
-                 [org.clojure/tools.trace "0.7.6"]])
+                 [org.clojure/tools.trace "0.7.6"]]
+  :main darkrogue.core)
