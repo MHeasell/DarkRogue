a2b23cee873be70c7a389e99b67cd22c2ab793d9
Michael Heasell <michael.heasell@gmail.com>
2014-03-14 22:50:50 +0100

    Add localrepo plugin and set up rlforj as a lein dependency

--- project.clj
+++ project.clj
@@ -5,5 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clojure-lanterna "0.9.4"]
-                 [org.clojure/tools.trace "0.7.6"]]
+                 [org.clojure/tools.trace "0.7.6"]
+                 [rlforj "0.3"]]
+  :plugins [[lein-localrepo "0.5.3"]]
   :main darkrogue.core)
